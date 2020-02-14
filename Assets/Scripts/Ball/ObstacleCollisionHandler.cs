c264bd67d07b69b00a1b0ef17f986a1d0a35de0f
Nick N <nicholas.ngoc.nguyen@gmail.com>
2020-02-13 22:54:34 -0700

    Explode on contact

---
+++ Assets/Scripts/Ball/ObstacleCollisionHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Simple class on the actual ball obj that causes an
+/// explosion when colliding with an obstacle
+/// 
+/// Note: For now the ball does not explode when in contact with the pins
+/// </summary>
+public class ObstacleCollisionHandler : MonoBehaviour
+{
+    public ExplosionControl explosionControl;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Obstacle"))
+        {
+            explosionControl.Explode();
+        }
+    }
+}

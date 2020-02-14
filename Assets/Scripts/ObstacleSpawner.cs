703593b15348803c9cf27003eb922064cbf4a491
Nick N <nicholas.ngoc.nguyen@gmail.com>
2020-02-13 22:34:35 -0700

    Added cone and obstacle spawner

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Simple class that spawns obstacles within a set radius
+/// around the obj this is attached to
+/// </summary>
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject[] obstaclePrefabs;    //Array of obstacles to spawn. Randomly chooses between objects in the array
+    public float spawnRadius;   //Radius from this classes obj to spawn obstacles
+    public float spawnRate; //How fast in seconds to spawn obstacles
+
+    private void Start()
+    {
+        StartSpawningObstacles();
+    }
+
+    public void StartSpawningObstacles()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    /// <summary>
+    /// Coroutine that continuously spawns obstacles
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator SpawnRoutine()
+    {
+        while(true)
+        {
+            //Random angle to determine where the obstacle will spawn around this gameobject
+            float randAngle = Random.Range(0, 2 * Mathf.PI);
+
+            Vector3 spawnPos = transform.position;
+            /**
+             * Got that calc
+             * x = r * cos(theta)
+             * y = r * sin(theta)
+             * Wait was that calc?
+             */
+            spawnPos.x = spawnRadius * Mathf.Cos(randAngle);
+            spawnPos.z = spawnRadius * Mathf.Sin(randAngle);
+
+            //Spawns object and waits to spawn a new random one
+            Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)], spawnPos, transform.rotation);
+            yield return new WaitForSeconds(spawnRate);
+        }
+    }
+}

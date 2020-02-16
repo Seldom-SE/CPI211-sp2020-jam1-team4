a8899be03f2bf7aca00ebb0d3b2689a02950d516
Seldom <ssaguUUUU@gmail.com>
2020-02-15 19:55:53 -0700

    Partial Score System

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pin : MonoBehaviour
+{
+    private bool fallen;
+    private ScoreController scoreObject;
+
+    private void Start()
+    {
+        scoreObject = GameObject.Find("Score").GetComponent<ScoreController>();
+    }
+
+    private void Update()
+    {
+        if (!fallen && gameObject.transform.rotation.eulerAngles.z < 300)
+        {
+            
+            fallen = true;
+            
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    private int pins;
+    private Text text;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    public void AddScore()
+    {
+        pins++;
+        text.text = "Score: " + pins * 100;
+    }
+}
